6b3b6cfabf0a46e4ef63af01122a894ed9aadfe7
Yash jain <161117850+yashjain15357@users.noreply.github.com>
2026-07-20 11:08:50 +0530

    Time: 0 ms (100%), Space: 11.8 MB (11.77%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,19 +1,38 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        unordered_map<int , int> mp;
-        for(int i : nums){
-            mp[i]++;
-        }
-        int k = 0;
-        for(int i = 0 ; i<3 ; i++){
-            int size = mp[i];
-            for(int j = 0 ; j<size ; j++){
-                nums[k] = i;
-                k++;
+        // unordered_map<int , int> mp;
+        // for(int i : nums){
+        //     mp[i]++;
+        // }
+        // int k = 0;
+        // for(int i = 0 ; i<3 ; i++){
+        //     int size = mp[i];
+        //     for(int j = 0 ; j<size ; j++){
+        //         nums[k] = i;
+        //         k++;
+        //     }
+        // }
+        
+        // three pointer method
+        int s = 0 ;
+        int e = nums.size()-1;
+        int m = 0;
+
+        while(m<=e){
+            if(nums[m]>1){
+                swap(nums[m] , nums[e]);
+                e--;
+            }
+            else if(nums[m] == 1){
+                m++;
+            }
+            else{
+                swap(nums[m] , nums[s]);
+                s++;
+                m++;
             }
         }
-        
         
     }
 };
