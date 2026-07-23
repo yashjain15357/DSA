d04c76740e47e23caa291930e688a109f2883d53
Yash jain <161117850+yashjain15357@users.noreply.github.com>
2026-07-23 12:46:02 +0530

    Time: 330 ms (5.02%), Space: 24.8 MB (10.56%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,17 +1,43 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        // if(intervals.size() <=0) return intervals;
+
+        // // 1. Sort intervals by starting time
+        // sort(intervals.begin(), intervals.end());
+        // vector<vector<int>> ans;
+        // // Start with the first interval
+        // ans.push_back(intervals[0]);
+
+
+        // for(int i = 1 ; i<intervals.size() ; i++){
+        //     if(intervals[i][0] <= ans.back()[1]){
+        //         ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+
+        //     }
+        //     else{
+        //         ans.push_back(intervals[i]);
+
+        //     }
+        // }
+        // return ans;
+
+
         if(intervals.size() <=0) return intervals;
 
         // 1. Sort intervals by starting time
         sort(intervals.begin(), intervals.end());
         vector<vector<int>> ans;
         // Start with the first interval
-        ans.push_back(intervals[0]);
-
+        // ans.push_back(intervals[0]);
+        if(intervals.size() <=1) return intervals;
+        
+        
         for(int i = 1 ; i<intervals.size() ; i++){
-            if(intervals[i][0] <= ans.back()[1]){
-                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+            if(intervals[i][0] <= intervals[i-1][1]){
+                intervals[i-1][1] = max(intervals[i-1][1], intervals[i][1]);
+                intervals.erase(intervals.begin()+i);
+                i--;
 
             }
             else{
@@ -19,7 +45,7 @@ public:
 
             }
         }
-        return ans;
+        return intervals;
 
 
 
