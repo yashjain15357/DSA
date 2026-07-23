1796a827ec37788c26c5044c69a9cbcf93148b01
Yash jain <161117850+yashjain15357@users.noreply.github.com>
2026-07-23 21:35:56 +0530

    Time: 3 ms (80.67%), Space: 11.7 MB (77%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,32 +1,52 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map <char , pair<int , bool> > mp;
-        int count = 0;
-        for(int i = 0 ; i<s.size() ; i++){
-            mp[s[i]].first = i;
-            mp[s[i]].second = true;
+        // unordered_map <char , pair<int , bool> > mp;
+        // int count = 0;
+        // for(int i = 0 ; i<s.size() ; i++){
+        //     mp[s[i]].first = i;
+        //     mp[s[i]].second = true;
 
             
-            int j = i+1;
-            int temp = 1;
-            while(j < s.size()){
-                if(!mp[s[j]].second){
-                    mp[s[j]].first = j;
-                    mp[s[j]].second = true;
-                    temp++;
-                }
-                else{
-                    i = mp[s[j]].first;
-                    mp.clear();
-                    break;
-                }
-                j++;
+        //     int j = i+1;
+        //     int temp = 1;
+        //     while(j < s.size()){
+        //         if(!mp[s[j]].second){
+        //             mp[s[j]].first = j;
+        //             mp[s[j]].second = true;
+        //             temp++;
+        //         }
+        //         else{
+        //             i = mp[s[j]].first;
+        //             mp.clear();
+        //             break;
+        //         }
+        //         j++;
                 
-            }
-            count = max(count , temp);
+        //     }
+        //     count = max(count , temp);
+        // }
+        // return count;
+
+        vector<int>arr(256 , -1);
+
+        int left = 0;
+        int maxlen = 0;
+
+
+        for(int right = 0 ; right<s.length(); right++){
+           unsigned char curr_char= s[right];
+           if(arr[curr_char]>=left){
+            left = arr[curr_char]+1;
+           }
+           arr[curr_char] = right;
+           maxlen = max(maxlen , right-left+1);
+            
+
         }
-        return count;
+        return maxlen;
+
+
 
 
         
